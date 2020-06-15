317a6c868c3ee0ba85d6e50301fb6a6d1310ef7e
Lukasz1928 <Lukasz19281@gmail.com>
2020-06-15 20:05:28 +0200

    First iaml setup

---
+++ app/src/main/java/com/sm/avatar/chatbot/iaml/IamlRequestTask.java
@@ -0,0 +1,15 @@
+package com.sm.avatar.chatbot.iaml;
+
+import android.os.AsyncTask;
+
+public class IamlRequestTask extends AsyncTask<Void, Void, String> {
+    @Override
+    protected String doInBackground(Void... voids) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String response) {
+
+    }
+}
